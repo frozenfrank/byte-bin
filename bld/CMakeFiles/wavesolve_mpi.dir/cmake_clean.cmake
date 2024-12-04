df2519334a3d113ddfdbc0fbb06d8220d5860229
Paige Despain <paigeid@byu.edu>
2024-12-04 10:57:22 -0700

    2024 phase 7

---
+++ bld/CMakeFiles/wavesolve_mpi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wavesolve_mpi.dir/wavesolve.cpp.o"
+  "CMakeFiles/wavesolve_mpi.dir/wavesolve.cpp.o.d"
+  "wavesolve_mpi"
+  "wavesolve_mpi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wavesolve_mpi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
