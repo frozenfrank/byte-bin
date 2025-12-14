5a60746fe871a52cba8dd75adc309dd90d159fb0
James Finlinson <the30clues@gmail.com>
2025-12-14 13:43:28 -0600

    Update access.js script to route request through a proxy

--- docs/delorean/toggl/access.js
+++ docs/delorean/toggl/access.js
@@ -1,11 +1,23 @@
-async function getTimeEntries(token) {
-  const response = await fetch("https://api.track.toggl.com/api/v9/me/time_entries", {
+async function getTimeEntries(token,proxy) {
+  // Fetch time entries from Toggl API
+  let requestURL = "https://api.track.toggl.com/api/v9/me/time_entries";
+  if (proxy) {
+    requestURL = `${proxy}?url=${encodeURIComponent(requestURL)}`;
+  }
+  const response = await fetch(requestURL, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       "Authorization": `${token}:api_token`,
     },
   });
+
+  // Interpret response
+  if (!response.ok) {
+    const responseText = await response.text();
+    if (responseText) console.error(responseText)
+    throw new Error(`Error fetching time entries: ${response.status} ${response.statusText}`);
+  }
   const data = await response.json();
   console.log("Retrieved time entries:", data);
 }
