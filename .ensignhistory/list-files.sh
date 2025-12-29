ccd2d30ef30ec49fb46d767fc8ffefcb7fa22b0a
James Finlinson <the30clues@gmail.com>
2025-12-29 09:01:36 -0700

    Convert to standard CSV format with inplace directories

--- .ensignhistory/list-files.sh
+++ .ensignhistory/list-files.sh
@@ -2,7 +2,9 @@
 
 list_files() {
     local dir="${1:-.}"
+    local outfile="$dir/.dirlist"
 
+    echo "Filename|Size|Last Modified" > $outfile
     for item in "$dir"/*; do
         if [ -e "$item" ]; then
             if [ -d "$item" ]; then
@@ -14,7 +16,7 @@ list_files() {
                 local filesize=$(stat -f%z "$item" 2>/dev/null || stat -c%s "$item" 2>/dev/null)
                 local timestamp=$(stat -f%Sm -t%Y-%m-%d\ %H:%M:%S "$item" 2>/dev/null || stat -c%y "$item" 2>/dev/null | cut -d' ' -f1,2)
 
-                echo "$filename | Size: $filesize | Modified: $timestamp"
+                echo "$filename|$filesize|$timestamp" >> $outfile
             fi
         fi
     done
