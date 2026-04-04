85172c8175a5c88047ad2f5f1583061bf0b4bfd1
James Finlinson <the30clues@gmail.com>
2026-04-04 17:06:18 -0500

    Detect changes and auto-source when necessary

--- bashrc.sh
+++ bashrc.sh
@@ -4,8 +4,14 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 # Check for updates
 if [ -d "$DOTFILES_DIR" ]; then
-    (
-        cd "$DOTFILES_DIR" &&
-        git pull --quiet
-    ) &
+    cd "$DOTFILES_DIR"
+
+    PREV_HASH=$(git rev-parse HEAD)
+    git pull --quiet
+    NEW_HASH=$(git rev-parse HEAD)
+
+    if [ "$PREV_HASH" != "$NEW_HASH" ]; then
+        echo "Changes detected! A pull occurred."
+        source bashrc.sh
+    fi
 fi
