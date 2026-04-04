b82e563b9aff4c7c509c756ca723e0a18b1cf51e
James Finlinson <the30clues@gmail.com>
2026-04-04 17:11:15 -0500

    Update git without changing directories

--- bashrc.sh
+++ bashrc.sh
@@ -4,14 +4,12 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 # Check for updates
 if [ -d "$DOTFILES_DIR" ]; then
-    cd "$DOTFILES_DIR"
-
-    PREV_HASH=$(git rev-parse HEAD)
-    git pull --quiet
-    NEW_HASH=$(git rev-parse HEAD)
+    PREV_HASH=$(git -C "$DOTFILES_DIR" rev-parse HEAD)
+    git -C "$DOTFILES_DIR" pull --quiet
+    NEW_HASH=$(git -C "$DOTFILES_DIR" rev-parse HEAD)
 
     if [ "$PREV_HASH" != "$NEW_HASH" ]; then
         echo "Changes detected! A pull occurred."
-        source bashrc.sh
+        source "$DOTFILES_DIR/bashrc.sh"
     fi
 fi
