706e5c2c5eea27b487bcf0dd34e01711a4d85de3
James Finlinson <the30clues@gmail.com>
2026-04-04 21:10:25 -0500

    Also automatically source a user's bash aliases

--- bash_profile.sh
+++ bash_profile.sh
@@ -5,3 +5,9 @@ ALIASES_FILE="$HOME/.dotfiles/.bash_aliases"
 if [ -f "$ALIASES_FILE" ]; then
     source "$ALIASES_FILE"
 fi
+
+ALIASES_FILE="$HOME/.bash_aliases"
+
+if [ -f "$ALIASES_FILE" ]; then
+    source "$ALIASES_FILE"
+fi
