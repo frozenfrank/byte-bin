9ec4e338561c5948cfca04283e6f9f2316e310af
James Finlinson <the30clues@gmail.com>
2026-06-03 00:49:10 -0500

    Remove all source files, except README.md

--- scripts/serve.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
-
-./scripts/build.sh
-./node_modules/http-server/bin/http-server dist/ -c-1
--- src/helper.ts
+++
@@ -1,4 +0,0 @@
-/** Helper function that assumes the value is element is always in the DOM. */
-export function getElementById<T = HTMLElement>(id: string): T {
-  return document.getElementById(id) as T;
-}
